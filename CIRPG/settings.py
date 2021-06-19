f377bda0dd96f4b32bd9f1724a03e515c834d2e9
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-06-19 14:54:10 +0100

    Add home app and set up templates.

--- CIRPG/settings.py
+++ CIRPG/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    # Custom Apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,10 @@ ROOT_URLCONF = 'CIRPG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
