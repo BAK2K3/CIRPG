076ec2b89f0d81e5e62928b5dace3e0b747eee6d
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-03 18:22:07 +0100

    Add and register CodexAdmin class.

--- codex/admin.py
+++ codex/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Codex
 
 
+class CodexAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'alpha_name',
+        'type',
+        'base_hp',
+        'base_attack',
+        'base_defense',
+        'base_speed',
+        'paid',
+        'min_level'
+    )
+
+    ordering = ('pk',)
+
+
 # Register the Codex model
-admin.site.register(Codex)
+admin.site.register(Codex, CodexAdmin)
