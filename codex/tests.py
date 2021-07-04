fb6473bcb8c90e15c1bf85dd8a94a65c82b9dc1c
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-04 09:36:11 +0100

    Add two test cases for codex views.

--- codex/tests.py
+++ codex/tests.py
@@ -1,3 +1,21 @@
+"""
+Codex App - Tests
+----------------
+
+Test cases for Codex App Routing
+"""
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def test_codex_page(self):
+        """ Test codex route renders correct page """
+        response = self.client.get('/codex/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'codex/codex.html')
+
+    def test_codex_context(self):
+        """ Test codex route queries DB """
+        response = self.client.get('/codex/')
+        self.assertTrue('codex' in response.context)
