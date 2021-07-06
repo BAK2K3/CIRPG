8fa1f3a97d3565d184a29fa0ab525bfeeb76b662
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-06 18:49:27 +0100

    Add db sort conditioning and add to context.

--- codex/views.py
+++ codex/views.py
@@ -4,18 +4,41 @@ from .models import Codex
 
 def codex(request):
     """
-    A view to return and filter all entries within codex.
+    A view to return all entries within codex.
+    View takes parameters for both sorting and searching.
+    These paremeters are then returned to the view for
+    template logic.
     """
     # Query DB for codex entries
     codex = Codex.objects.all()
 
-    # Initialise Filter Params
+    # Initialise Sort/Filter Params
     premium = None
     entry_type = None
     levels = None
     search_params = {}
+    sort_by = 'pk'
+    sort_dir = None
+    sort_params = sort_by
 
     if request.GET:
+        # If sort criteria has been specified
+        if 'sort' in request.GET:
+            if request.GET['sort'] in ['name', 'type', 'base_hp',
+                                       'base_attack', 'base_defense',
+                                       'base_speed']:
+                sort_by = request.GET['sort']
+                sort_params = sort_by
+
+        # If sort direction has been specified
+        if 'direction' in request.GET:
+            if request.GET['direction'] == 'desc':
+                sort_dir = request.GET['direction']
+                sort_params = f'-{sort_by}'
+
+        # Perform sort functionality
+        codex = codex.order_by(sort_params)
+
         # If Premium filter has been applied
         if 'premium' in request.GET:
             if request.GET['premium'] in ['0', '1']:
@@ -36,7 +59,7 @@ def codex(request):
             except ValueError as e:
                 print(e)
 
-        # If any search parameters exist, apply the filter
+        # If any filter parameters exist, apply the filter
         if len(search_params) > 0:
             codex = codex.filter(**search_params)
 
@@ -45,7 +68,9 @@ def codex(request):
         'codex': codex,
         'premium': premium,
         'entry_type': entry_type,
-        'levels': levels
+        'levels': levels,
+        'sort_by': sort_by,
+        'sort_dir': sort_dir,
     }
     template = 'codex/codex.html'
     return render(request, template, context)
