29a1394949d1e4fba7fc46c1eceb20e6d3fcd429
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-08 18:54:57 +0100

    Refactor Codex view into Class Based View.

--- codex/urls.py
+++ codex/urls.py
@@ -5,9 +5,9 @@ Codex App - URLS
 URL Routing for the Codex App
 """
 
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('', views.codex, name="codex"),
+    re_path(r'$', views.CodexListView.as_view(), name='codex'),
 ]
--- codex/views.py
+++ codex/views.py
@@ -1,76 +1,87 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 from .models import Codex
 
 
-def codex(request):
+class CodexListView(ListView):
     """
     A view to return all entries within codex.
     View takes parameters for both sorting and searching.
     These paremeters are then returned to the view for
     template logic.
     """
-    # Query DB for codex entries
-    codex = Codex.objects.all()
 
-    # Initialise Sort/Filter Params
-    premium = None
-    entry_type = None
-    levels = None
-    search_params = {}
-    sort_by = 'pk'
-    sort_dir = None
-    sort_params = sort_by
+    # Override default model/template/context name
+    model = Codex
+    template_name = 'codex/codex.html'
+    context_object_name = 'codex'
 
-    if request.GET:
+    # Override get_queryset
+    def get_queryset(self):
+        # Extract context parameters from request
+        self.prepared_context = self.process_codex_url(self.request.GET)
+        # Apply search and sort params to queryset
+        qs = (
+                super().get_queryset()
+                .filter(**self.prepared_context['search_params'])
+                .order_by(self.prepared_context['sort_params'])
+        )
+        return qs
+
+    # Override get_queryset
+    def get_context_data(self, **kwargs):
+        # Merge default context with prepared context params
+        context = super().get_context_data(**kwargs) | self.prepared_context
+        return context
+
+    # Function for processing GET kwargs
+    # Need to consider where to move this to
+    def process_codex_url(self, get_params):
+
+        # Set default context dict
+        context = {
+                'premium': None,
+                'entry_type': None,
+                'levels': None,
+                'search_params': {},
+                'sort_by': 'pk',
+                'sort_dir': None,
+                'sort_params': 'pk',
+            }
+
         # If sort criteria has been specified
-        if 'sort' in request.GET:
-            if request.GET['sort'] in ['name', 'type', 'base_hp',
-                                       'base_attack', 'base_defense',
-                                       'base_speed']:
-                sort_by = request.GET['sort']
-                sort_params = sort_by
+        if 'sort' in get_params:
+            if get_params['sort'] in ['name', 'type', 'base_hp',
+                                      'base_attack', 'base_defense',
+                                      'base_speed']:
+                context['sort_by'] = get_params['sort']
+                context['sort_params'] = context['sort_by']
 
         # If sort direction has been specified
-        if 'direction' in request.GET:
-            if request.GET['direction'] == 'desc':
-                sort_dir = request.GET['direction']
-                sort_params = f'-{sort_by}'
-
-        # Perform sort functionality
-        codex = codex.order_by(sort_params)
+        if 'direction' in get_params:
+            if get_params['direction'] == 'desc':
+                context['sort_dir'] = get_params['direction']
+                context['sort_params'] = f"-{context['sort_by']}"
 
         # If Premium filter has been applied
-        if 'premium' in request.GET:
-            if request.GET['premium'] in ['0', '1']:
-                premium = bool(int(request.GET['premium']))
-                codex = codex.filter(paid__exact=premium)
+        if 'premium' in get_params:
+            if get_params['premium'] in ['0', '1']:
+                context['premium'] = bool(int(get_params['premium']))
+                context['search_params']['paid__exact'] = context['premium']
 
         # If Type filter has been applied
-        if 'type' in request.GET:
-            if request.GET['type'] in ['weapon', 'enemy', 'hero']:
-                entry_type = request.GET['type']
-                search_params['type__icontains'] = entry_type
+        if 'type' in get_params:
+            if get_params['type'] in ['weapon', 'enemy', 'hero']:
+                type_arg = get_params['type']
+                context['entry_type'] = type_arg
+                context['search_params']['type__icontains'] = type_arg
 
         # If min_level filter has been applied, and not hero type
-        if 'levels' in request.GET and entry_type != "hero":
+        if 'levels' in get_params and context['entry_type'] != "hero":
             try:
-                levels = [int(x) for x in request.GET['levels'].split(',')]
-                search_params['min_level__in'] = levels
+                level_arg = [int(x) for x in get_params['levels'].split(',')]
+                context['levels'] = level_arg
+                context['search_params']['min_level__in'] = level_arg
             except ValueError as e:
                 print(e)
 
-        # If any filter parameters exist, apply the filter
-        if len(search_params) > 0:
-            codex = codex.filter(**search_params)
-
-    # Add all queries to context
-    context = {
-        'codex': codex,
-        'premium': premium,
-        'entry_type': entry_type,
-        'levels': levels,
-        'sort_by': sort_by,
-        'sort_dir': sort_dir,
-    }
-    template = 'codex/codex.html'
-    return render(request, template, context)
+        return context
