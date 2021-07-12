67b34fc10d4688a8e7f50bd941414793612c072f
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-12 16:33:32 +0100

    Add Profile CBV to Profiles views.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Profile
 
-# Create your views here.
+
+class ProfileDetailView(LoginRequiredMixin, ListView):
+    context_object_name = 'profile'
+    template_name = "profiles/profile.html"
+
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
