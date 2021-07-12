4fc6206fd3f0e198b67e47d089e4937f9d6f2cb0
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-12 16:32:12 +0100

    Link Profile to Admin View.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+# Admin class for Codex
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Profile Admin settiings for list display and ordering.
+    """
+    list_display = (
+        'pk',
+        'user',
+        'paid',
+        'active_char',
+        'active_battle',
+        'total_runs',
+        'longest_run',
+    )
+
+    ordering = ('pk',)
+
+
+# Register the Codex model
+admin.site.register(Profile, ProfileAdmin)
