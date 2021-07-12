5ba126c63ecd2b5f8071c9d3bd0ba5268c1f5d8f
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-12 16:32:36 +0100

    Link profile urls to project level urls.

--- CIRPG/urls.py
+++ CIRPG/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('codex/', include('codex.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
