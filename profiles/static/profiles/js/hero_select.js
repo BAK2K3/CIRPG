3888a286f932f56be5850be28185bb07fcd39bd1
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-13 19:49:19 +0100

    Add underline styling to hero select.

--- profiles/static/profiles/js/hero_select.js
+++ profiles/static/profiles/js/hero_select.js
@@ -1,17 +1,14 @@
 
 $(document).ready(function () {
-    
     $(".card").click(function() {
-    
         if (!$(this).hasClass("selected-card")) {
             $(".card").removeClass("selected-card");
+            $(".card .card-body h5").removeClass("text-decoration-underline")
             $(this).addClass("selected-card");
+            $(this).children(".card-body").children("h5").addClass("text-decoration-underline")
             $("#HeroForm input").val = $(this).attr("id").slice(0,-4);
             $("#HeroForm button span").html("Create Character");
             $("#HeroForm button").prop("disabled", false);
-
         }
     });
-
-
 });
