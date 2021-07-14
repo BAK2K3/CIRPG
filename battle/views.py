6f0f6ad7f4e9215aab24c4a67909b950d35c0582
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-14 22:44:39 +0100

    Add BattleView for serving char/enemy.

--- battle/views.py
+++ battle/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from profiles.models import ActiveCharacter
+from battle.models import ActiveEnemy
 
-# Create your views here.
+
+class BattleView(TemplateView):
+    template_name = "battle/battle.html"
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        active_character = ActiveCharacter.objects.get(user=self.request.user)
+        context_data['character'] = active_character
+        context_data['enemy'] = ActiveEnemy.objects.get(
+            active_character=active_character
+            )
+        return context_data
