fa181df66751d984e95c277b98d81b49ec8ac435
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-14 21:09:56 +0100

    Link Active Enemy to admin.

--- battle/admin.py
+++ battle/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import ActiveEnemy
 
-# Register your models here.
+
+# Admin class for Active Enemy
+class EnemyAdmin(admin.ModelAdmin):
+    """
+    Profile Admin settiings for list display and ordering.
+    """
+    list_display = (
+        'pk',
+        'active_character',
+        'enemy_id',
+        'enemy_level',
+        'weapon_id',
+        'weapon_level'
+    )
+
+    ordering = ('pk',)
+
+
+# Register the models
+admin.site.register(ActiveEnemy, EnemyAdmin)
