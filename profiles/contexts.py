25d75e1c014d65acfdbe6d1398d3adba6fe9482d
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-14 12:51:51 +0100

    Add custom context for storing profile.

---
+++ profiles/contexts.py
@@ -0,0 +1,15 @@
+from profiles.models import Profile
+
+
+def active_user(request):
+    if request.user.is_authenticated:
+        current_profile = Profile.objects.get(user=request.user)
+        request.session['user_paid'] = current_profile.paid
+    else:
+        current_profile = None
+
+    context = {
+        'current_profile': current_profile
+    }
+
+    return context
