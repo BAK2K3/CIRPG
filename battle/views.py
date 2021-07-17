98c7080670f453f242c3352eb913dd223ed08ff9
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-17 13:00:30 +0100

    Add Post Battle Route skeleton.

--- battle/views.py
+++ battle/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from profiles.models import ActiveCharacter
 from battle.models import ActiveEnemy
 from django.core import serializers
+from django.contrib.auth.mixins import LoginRequiredMixin
+import json
 
 
 class BattleView(TemplateView):
@@ -22,3 +25,31 @@ class BattleView(TemplateView):
         data['json_char'] = serializers.serialize('json', [data['character']])
         data['json_enemy'] = serializers.serialize('json', [data['enemy']])
         return data
+
+
+class PostBattleView(LoginRequiredMixin, TemplateView):
+    """
+    A View for post-battle page.
+
+    View changes depending on outcome of battle.
+    
+    Outcome of battle influences whether level up
+    and new loot is obtained, or whether character
+    is deleted.
+
+    Active Enemy is deleted regardless.
+    """
+    template_name = 'battle/post_battle.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['outcome'] = json.loads(self.request.POST['result'])
+
+        return context
+
+    def post(self, request, **kwargs):
+        context = self.get_context_data()
+        return super(PostBattleView, self).render_to_response(context)
+
+    def get(self, request, **kwargs):
+        return redirect('profile')
