c12630be08e06cef1d523a8591553db6d81d9bc9
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-18 13:26:13 +0100

    Add Leaderboard to admin.

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Leaderboard
 
-# Register your models here.
+
+# Admin class for Profile
+class LeaderboardAdmin(admin.ModelAdmin):
+    """
+    Leaderboard Admin settings for list display and ordering.
+    """
+    list_display = (
+        'user',
+        'score',
+    )
+
+    ordering = ('-score',)
+
+
+# Register the models
+admin.site.register(Leaderboard, LeaderboardAdmin)
