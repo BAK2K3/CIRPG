64f4b479b7a04be52fee926dbb044a7a6232f9d1
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-18 13:43:46 +0100

    Change TemplateView to ListView with custom query.

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Leaderboard
 
 
-class LeaderboardView(TemplateView):
+class LeaderboardView(ListView):
     """
     A View for the Leaderboard Page.
 
     This view presents the top ten scored to the user.
     """
+    model = Leaderboard
     template_name = 'leaderboard/leaderboard.html'
+    context_object_name = 'leaderboard'
+
+    # Override get_queryset
+    def get_queryset(self):
+        # Obtain sorted leaderboard
+        return self.model.objects.sort_leaderboard()
