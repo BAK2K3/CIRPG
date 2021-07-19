031374b2d5d66fb9f394c33ecee10d935f8c2c82
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-19 19:44:31 +0100

    Add AJAX stripe config handler.

--- premium/views.py
+++ premium/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 class PremiumView(TemplateView):
@@ -6,3 +9,13 @@ class PremiumView(TemplateView):
     Template View for Premium Page
     """
     template_name = "premium/premium.html"
+
+
+@csrf_exempt
+def stripe_config(request):
+    """
+    AJAX Get handler for stripe public key.
+    """
+    if request.method == 'GET':
+        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+        return JsonResponse(stripe_config, safe=False)
