7d204bb8ed7f98e977d2196855a009a10ae0df29
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-19 18:10:44 +0100

    Add unit test for premium page view.

--- premium/tests.py
+++ premium/tests.py
@@ -1,3 +1,16 @@
+"""
+Premium App - Tests
+----------------
+
+Test cases for Premium App Routing
+"""
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def test_premium_page(self):
+        """ Test premium page renders correct page """
+        response = self.client.get('/premium/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'premium/premium.html')
