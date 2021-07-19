cd3d1818ba29e12057119671c73d34d83207048b
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-19 19:43:38 +0100

    Add unit test for JSON stripe config.

--- premium/tests.py
+++ premium/tests.py
@@ -6,6 +6,7 @@ Test cases for Premium App Routing
 """
 
 from django.test import TestCase
+from django.conf import settings
 
 
 class TestViews(TestCase):
@@ -14,3 +15,10 @@ class TestViews(TestCase):
         response = self.client.get('/premium/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'premium/premium.html')
+
+    def test_stripe_config(self):
+        """Test stripe public key view"""
+        response = self.client.get('/premium/config/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'),
+                             {'publicKey': settings.STRIPE_PUBLISHABLE_KEY})
