d0787f5c81d89699611da03e44657deb0465029a
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-19 18:11:37 +0100

    Add Premium view URL to app level urls.

---
+++ premium/urls.py
@@ -0,0 +1,13 @@
+"""
+Premium App - URLS
+---------------
+
+URL Routing for the Home App
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PremiumView.as_view(), name="premium"),
+]
