ca3e9579b5586576ab5f3b5a99544b26700ce7f1
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-19 19:44:04 +0100

    Add stripe env variables to settings.

--- CIRPG/settings.py
+++ CIRPG/settings.py
@@ -170,3 +170,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe env variables
+STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_PRICE_ID = os.getenv("STRIPE_PRICE_ID")
