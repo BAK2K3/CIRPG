4137064eaf61dfb91604aa5b5867dfae2bded936
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-20 18:54:58 +0100

    Add set up and checkout error unit tests.

--- premium/tests.py
+++ premium/tests.py
@@ -8,9 +8,26 @@ Test cases for Premium App Routing
 from django.test import TestCase
 from django.conf import settings
 import json
+from profiles.models import Profile
+from django.contrib.auth import get_user_model
 
 
 class TestViews(TestCase):
+
+    def setUp(self):
+        """ Create test login user and create Profile entry"""
+        username = "Ben"
+        pswd = "Kavanagh" # noqa
+        User = get_user_model()
+        self.user = User.objects.create_user(username=username,
+                                             password=pswd)
+        logged_in = self.client.login(username=username, password=pswd)
+
+        # Add User to Profile
+        # Needed to be hardcoded as Profile signal uses email verification
+        self.profile = Profile.objects.create(user=self.user)
+        self.assertTrue(logged_in)
+
     def test_premium_page(self):
         """ Test premium page renders correct page """
         response = self.client.get('/premium/')
@@ -29,6 +46,14 @@ class TestViews(TestCase):
         response = self.client.get('/premium/checkout/')
         json_response = json.loads(str(response.content, encoding='utf8'))
         self.assertTrue('sessionId' in json_response)
+
+    def test_premium_checkout_error(self):
+        """ Test AJAX checkout session view returns sessionId"""
+        self.profile.paid = True
+        self.profile.save()
+        response = self.client.get('/premium/checkout/')
+        json_response = json.loads(str(response.content, encoding='utf8'))
+        self.assertTrue('error' in json_response)
 
     def test_success_page(self):
         """ Test successful payment page renders correct page """
