1165595453aea034c8dc58f5733221a240fd4c9b
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-24 16:21:59 +0100

    Link Delete and Create views to urls.

--- codex/urls.py
+++ codex/urls.py
@@ -9,6 +9,10 @@ from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
+    path('delete/<slug:pk>/',
+         views.CodexDeleteView.as_view(),
+         name='c_delete'),
     path('edit/<slug:pk>/', views.CodexUpdateView.as_view(), name='c_edit'),
+    path('create/', views.CodexCreateView.as_view(), name='c_create'),
     re_path(r'$', views.CodexListView.as_view(), name='codex')
 ]
