3dbf998cb1769e066f997d958227d1d3e2f9a578
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-24 13:26:02 +0100

    Link edit view to urls.

--- codex/urls.py
+++ codex/urls.py
@@ -5,9 +5,10 @@ Codex App - URLS
 URL Routing for the Codex App
 """
 
-from django.urls import re_path
+from django.urls import re_path, path
 from . import views
 
 urlpatterns = [
-    re_path(r'$', views.CodexListView.as_view(), name='codex'),
+    path('edit/<slug:pk>/', views.CodexUpdateView.as_view(), name='c_edit'),
+    re_path(r'$', views.CodexListView.as_view(), name='codex')
 ]
