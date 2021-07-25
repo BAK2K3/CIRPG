09bc36332423bf86667d54d14f2259e9076b1379
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-25 13:55:25 +0100

    Add prints for testing.

--- profiles/contexts.py
+++ profiles/contexts.py
@@ -2,15 +2,20 @@ from profiles.models import Profile
 
 
 def active_user(request):
+    print("CONTEXT PROCESSOR WORKING")
     if request.user.is_authenticated:
+        print("AUTHENTICATED")
         current_profile = Profile.objects.get(user=request.user)
         request.session['user_paid'] = current_profile.paid
     else:
+        print("NOT AUTHENTICATED")
         current_profile = None
         request.session['user_paid'] = False
     current_profile = None
     context = {
         'current_profile': current_profile
     }
+
+    print(f"Context: {context}")
 
     return context
