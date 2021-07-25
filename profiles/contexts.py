32fbc9c26c132096b3f755882531374ca9b2f6fb
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-25 12:55:54 +0100

    Remove context temporarily for initial access.

--- profiles/contexts.py
+++ profiles/contexts.py
@@ -1,16 +1,16 @@
 from profiles.models import Profile
 
 
-def active_user(request):
-    if request.user.is_authenticated:
-        current_profile = Profile.objects.get(user=request.user)
-        request.session['user_paid'] = current_profile.paid
-    else:
-        current_profile = None
-        request.session['user_paid'] = False
+# def active_user(request):
+#     if request.user.is_authenticated:
+#         current_profile = Profile.objects.get(user=request.user)
+#         request.session['user_paid'] = current_profile.paid
+#     else:
+#         current_profile = None
+#         request.session['user_paid'] = False
 
-    context = {
-        'current_profile': current_profile
-    }
+#     context = {
+#         'current_profile': current_profile
+#     }
 
-    return context
+#     return context
