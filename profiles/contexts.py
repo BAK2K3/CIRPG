37ae8ddd8f2aa6e7c2bdc61ffa67ae431ccbd95e
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-25 14:05:55 +0100

    Remove debug, remove none bug, add docstring.

--- profiles/contexts.py
+++ profiles/contexts.py
@@ -2,20 +2,17 @@ from profiles.models import Profile
 
 
 def active_user(request):
-    print("CONTEXT PROCESSOR WORKING")
+    """
+    Context Processor for passing
+    Profile contexts to templates.
+    """
     if request.user.is_authenticated:
-        print("AUTHENTICATED")
         current_profile = Profile.objects.get(user=request.user)
         request.session['user_paid'] = current_profile.paid
     else:
-        print("NOT AUTHENTICATED")
         current_profile = None
         request.session['user_paid'] = False
-    current_profile = None
     context = {
         'current_profile': current_profile
     }
-
-    print(f"Context: {context}")
-
     return context
