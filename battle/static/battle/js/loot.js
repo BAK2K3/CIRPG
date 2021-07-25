3baa00f247148350f5ee04790e94437226d9d193
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-25 08:46:56 +0100

    Add window closure prevention.

--- battle/static/battle/js/loot.js
+++ battle/static/battle/js/loot.js
@@ -1,35 +1,48 @@
-    // Extract csrf and newly generated weapon json
-    const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    const newWeapon = document.getElementById('id_new_weapon').textContent; 
+window.addEventListener('beforeunload', preventClosure);
 
-    // Remove these elements from the DOM
-    document.querySelector('[name=csrfmiddlewaretoken]').remove();
-    document.getElementById('id_new_weapon').remove();
+function preventClosure(event){
+    // Cancel the event as stated by the standard.
+    event.preventDefault();
+    // Chrome requires returnValue to be set.
+    event.returnValue = 'Are you sure you want to exit?';
+}
 
-    // Ajax request for when the user clicks on "New Weapon" 
-    // Submits the newWeapon dictionary back to Loot view
-    // To save to the DB.
-    // Also amends the DOM to reflect users.
-    $("#newButton").click(function() {
-        $.ajax({
-            type: "POST",
-            url: "/battle/loot/",
-            headers: {'X-CSRFToken': csrf},
-            data: {
-                'newWeapon': newWeapon
-            },
-            success: function(){
-                $(".loot-button-container").addClass("d-none");
-                $("#oldWeapon").addClass("d-none");
-                $(".continue-button-container").removeClass("d-none");
-            }
-        });
+    
+// Extract csrf and newly generated weapon json
+const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value;
+const newWeapon = document.getElementById('id_new_weapon').textContent; 
+
+// Remove these elements from the DOM
+document.querySelector('[name=csrfmiddlewaretoken]').remove();
+document.getElementById('id_new_weapon').remove();
+
+// Ajax request for when the user clicks on "New Weapon" 
+// Submits the newWeapon dictionary back to Loot view
+// To save to the DB.
+// Also amends the DOM to reflect users.
+$("#newButton").click(function() {
+    $.ajax({
+        type: "POST",
+        url: "/battle/loot/",
+        headers: {'X-CSRFToken': csrf},
+        data: {
+            'newWeapon': newWeapon
+        },
+        success: function(){
+            $(".loot-button-container").addClass("d-none");
+            $("#oldWeapon").addClass("d-none");
+            $(".continue-button-container").removeClass("d-none");
+            window.removeEventListener("beforeunload", preventClosure);
+        }
     });
+});
 
-    // Function to remove "new weapon" button, remove new weapon
-    // Container, and add "continue" button container
-    $("#keepButton").click(function() {
-        $(".loot-button-container").addClass("d-none");
-        $("#newWeapon").addClass("d-none");
-        $(".continue-button-container").removeClass("d-none");
-    });
+// Function to remove "new weapon" button, remove new weapon
+// Container, and add "continue" button container
+$("#keepButton").click(function() {
+    $(".loot-button-container").addClass("d-none");
+    $("#newWeapon").addClass("d-none");
+    $(".continue-button-container").removeClass("d-none");
+});
+
+    
