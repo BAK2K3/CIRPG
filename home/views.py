96c7323d353a845eb62307025d60e912db05defb
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-07-29 18:37:33 +0100

    Add CBV for help page.

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     """ A view to return index page """
     template_name = 'home/index.html'
+
+
+class HelpView(TemplateView):
+    """A view to return Help page """
+    template_name = 'home/help.html'
