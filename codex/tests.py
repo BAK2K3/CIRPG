2c4c88f02e412febb63bbf58de2475986496e288
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-16 12:32:17 +0100

    Add test numbers to codex Unit Tests.

--- codex/tests.py
+++ codex/tests.py
@@ -34,66 +34,66 @@ class TestViews(TestCase):
         self.assertTrue(logged_in)
 
     def test_codex_page(self):
-        """ Test codex route renders correct page """
+        """ UT23 - Test codex route renders correct page """
         response = self.client.get('/codex/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'codex/codex.html')
 
     def test_codex_context(self):
-        """ Test codex route queries DB """
+        """ UT24 - Test codex route queries DB """
         response = self.client.get('/codex/')
         self.assertTrue('codex' in response.context)
 
     def test_codex_entry_structure(self):
-        """ Test codex db entry structure"""
+        """ UT25 - Test codex db entry structure"""
         response = self.client.get('/codex/')
         self.assertTrue(isinstance(response.context['codex'][0], Codex))
 
     def test_codex_query_premium(self):
-        """Test codex filtering for premium products """
+        """ UT26 - Test codex filtering for premium products """
         response = self.client.get('/codex/?premium=1')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.paid)
 
     def test_codex_query_free(self):
-        """Test codex filtering for free products """
+        """ UT27 - Test codex filtering for free products """
         response = self.client.get('/codex/?premium=0')
         codex_entry = response.context['codex'][0]
         self.assertFalse(codex_entry.paid)
 
     def test_codex_query_weapon(self):
-        """Test codex filtering for weapons """
+        """ UT28 - Test codex filtering for weapons """
         response = self.client.get('/codex/?type=weapon')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.type == "Weapon")
 
     def test_codex_query_enemies(self):
-        """Test codex filtering for enemies """
+        """ UT29 - Test codex filtering for enemies """
         response = self.client.get('/codex/?type=enemy')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.type == "Enemy")
 
     def test_codex_query_heroes(self):
-        """Test codex filtering for heroes """
+        """ UT30 - Test codex filtering for heroes """
         response = self.client.get('/codex/?type=hero')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.type == "Hero")
 
     def test_codex_query_level(self):
-        """Test codex filtering for rating"""
-        random_rating = str(randint(1, 5))
+        """ UT31 - Test codex filtering for rating """
+        random_rating = str(randint(1, 5))  # nosec
         response = self.client.get(f'/codex/?levels={random_rating}')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.min_level == int(random_rating))
 
     def test_codex_query_multilevel(self):
-        """Test codex filtering for multiple ratings"""
+        """ UT32 - Test codex filtering for multiple ratings """
         response = self.client.get('/codex/?levels=2%2C3')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.min_level in [2, 3])
 
     def test_codex_multi_query(self):
-        """Test codex filtering for multiple parameters"""
+        """ UT33 - Test codex filtering for multiple parameters """
         response = self.client.get('/codex/?premium=0&type=weapon&levels=1')
         codex_entry = response.context['codex'][0]
         self.assertTrue(codex_entry.min_level == 1)
@@ -101,7 +101,7 @@ class TestViews(TestCase):
         self.assertTrue(codex_entry.type == "Weapon")
 
     def test_codex_sort_dir(self):
-        """Test codex sorting direction params"""
+        """ UT34 - Test codex sorting direction parameters """
         response = self.client.get('/codex/?direction=desc')
         sort_dir = response.context['sort_dir']
         codex = response.context['codex']
@@ -110,7 +110,7 @@ class TestViews(TestCase):
         self.assertTrue(codex[0].pk == codex_length)
 
     def test_codex_sort_by(self):
-        """Test codex sort by params"""
+        """ UT35 - Test codex sort by parameters """
         response = self.client.get('/codex/?sort=base_hp')
         sort_by = response.context['sort_by']
         codex = response.context['codex']
@@ -120,7 +120,7 @@ class TestViews(TestCase):
         self.assertTrue(first_entry.base_hp < last_entry.base_hp)
 
     def test_codex_edit_view(self):
-        """Test codex edit view"""
+        """ UT36 - Test codex Edit view """
         # Set user to superuser
         self.user.is_superuser = True
         self.user.save()
@@ -134,7 +134,7 @@ class TestViews(TestCase):
         self.assertTrue(response.context['object'].pk == 1)
 
     def test_codex_create_view(self):
-        """Test codex Create view"""
+        """ UT37 - Test codex Create view """
         # Set user to superuser
         self.user.is_superuser = True
         self.user.save()
@@ -147,7 +147,7 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'codex/create.html')
 
     def test_codex_delete_view(self):
-        """Test codex Delete view"""
+        """ UT38 - Test codex Delete view """
         # Set user to superuser
         self.user.is_superuser = True
         self.user.save()
