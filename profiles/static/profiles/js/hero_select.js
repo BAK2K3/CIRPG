c5815545a7058982962ee518d37ee4db316d7346
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-16 14:51:29 +0100

    Change id_user_selection value to div title.

--- profiles/static/profiles/js/hero_select.js
+++ profiles/static/profiles/js/hero_select.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
             $(".card .card-body h5").removeClass("text-decoration-underline")
             $(this).addClass("selected-card");
             $(this).children(".card-body").children("h5").addClass("text-decoration-underline")
-            $("#id_user_selection").val($(this).attr("id").slice(0,-4));
+            $("#id_user_selection").val($(this).attr("title"));
             $("#HeroForm button span").html("Create Character");
             $("#HeroForm button").prop("disabled", false);
         }
