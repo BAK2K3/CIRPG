8d9b64a1299b6226664eb67b706cafb4d37f7031
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-16 08:08:52 +0100

    Add unit test numbers to home tests.

--- home/tests.py
+++ home/tests.py
@@ -10,13 +10,13 @@ from django.test import TestCase
 
 class TestViews(TestCase):
     def test_home_page(self):
-        """ Test home page renders correct page """
+        """ UT01 - Test home page renders correct page """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
 
     def test_help_page(self):
-        """ Test help page renders correct page """
+        """ UT02 - Test help page renders correct page """
         response = self.client.get('/help/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/help.html')
