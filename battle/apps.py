11a9620471e506d100a0abaf33d7409f9583aa7c
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-17 15:44:27 +0100

    Add module and class docstrings.

--- battle/apps.py
+++ battle/apps.py
@@ -1,6 +1,15 @@
+"""
+Battle App - Apps
+----------------
+
+App Configuration for Battle App.
+"""
+
+
 from django.apps import AppConfig
 
 
 class BattleConfig(AppConfig):
+    """Battle App configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'battle'
