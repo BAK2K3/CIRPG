130be69901c1f91728f40c7dfc70037255b9837c
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-17 13:39:21 +0100

    Add missing semicolons.

--- profiles/static/profiles/js/hero_select.js
+++ profiles/static/profiles/js/hero_select.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     $(".card").click(function() {
         if (!$(this).hasClass("selected-card")) {
             $(".card").removeClass("selected-card");
-            $(".card .card-body h5").removeClass("text-decoration-underline")
+            $(".card .card-body h5").removeClass("text-decoration-underline");
             $(this).addClass("selected-card");
-            $(this).children(".card-body").children("h5").addClass("text-decoration-underline")
+            $(this).children(".card-body").children("h5").addClass("text-decoration-underline");
             $("#id_user_selection").val($(this).attr("title"));
             $("#HeroForm button span").html("Create Character");
             $("#HeroForm button").prop("disabled", false);
