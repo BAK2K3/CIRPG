6f224df0c4b485161ab5aaf9b940527627c037b5
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-18 10:00:17 +0100

    Add module and class docstrings to codex apps.

--- codex/apps.py
+++ codex/apps.py
@@ -1,6 +1,14 @@
+"""
+Codex App - Apps
+----------------
+
+App Configuration for Codex App.
+"""
+
 from django.apps import AppConfig
 
 
 class CodexConfig(AppConfig):
+    """Codex App configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'codex'
