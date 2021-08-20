8a82c4710821a23f41e9928e25d9ce03b3e8acf6
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-20 19:47:42 +0100

    Add docstrings to widgets.

--- codex/widgets.py
+++ codex/widgets.py
@@ -1,9 +1,21 @@
+"""
+Codex App - Widgets
+----------------
+
+Custom HTML Widgets for Codex App.
+    - CustomClearableFileInput
+
+"""
+
 from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
-
+    """
+    Custom ClearableFileInput widget to specify initial text,
+    input text, and custom HTML template.
+    """
     initial_text = _('Current Image')
     input_text = _('')
     template_name = (
