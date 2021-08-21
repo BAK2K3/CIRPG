297ec5041d5e5de41db2b29f225100f1b55c9c20
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 13:05:26 +0100

    Add docstrings.

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,19 @@
+"""
+Custom Storages
+----------------
+
+Custom Storages for AWS.
+"""
+
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """Sets location for Static Storage."""
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """Sets location for Media Storage."""
     location = settings.MEDIAFILES_LOCATION
