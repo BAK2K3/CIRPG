4162704cf3ec5db29c35261bd6c7586cf84f88e6
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 08:04:37 +0100

    Add module and class docstrings.

--- premium/apps.py
+++ premium/apps.py
@@ -1,6 +1,14 @@
+"""
+Premium App - Apps
+----------------
+
+App Configuration for Premium App.
+"""
+
 from django.apps import AppConfig
 
 
 class PremiumConfig(AppConfig):
+    """Premium App configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'premium'
