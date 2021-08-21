2b548c9d7d24368a7231f06a58edf25cabcbf93e
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 14:44:51 +0100

    Add superuser profile creation signal.

--- profiles/signals.py
+++ profiles/signals.py
@@ -4,12 +4,17 @@ Profiles App - Signals
 
 Signals for Profiles App.
     - create_profile
+    - admin_profile
 """
 
 from allauth.account.signals import email_confirmed
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 from .models import Profile
+
+
+USER_MODEL = get_user_model()
 
 
 @receiver(email_confirmed)
@@ -18,3 +23,10 @@ def create_profile(request, email_address, **kwargs):
     user_model = get_user_model()
     user = user_model.objects.get(emailaddress=email_address)
     Profile.objects.create(user=user)
+
+
+@receiver(post_save, sender=USER_MODEL)
+def admin_profile(instance, created, **kwargs):
+    """ Create a Profile when a Superuser is created """
+    if created and instance.is_superuser:
+        Profile.objects.create(user=instance)
