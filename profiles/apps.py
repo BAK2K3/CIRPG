b4848bb60a210979066e824e45459b04165b2c98
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 11:49:39 +0100

    Add docstrings and pylint disable.

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,9 +1,20 @@
+"""
+Profiles App - Apps
+----------------
+
+App Configuration for Profiles App.
+"""
+
+# pylint: disable=C0415,W0611
+
 from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    """Profiles App configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
 
     def ready(self):
+        """Import signals when configured."""
         import profiles.signals # noqa
