51064b2af126971572b90213a173d928c8a8d30d
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 12:08:08 +0100

    Add docstring and arrange imports.

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,13 +1,20 @@
-from django.dispatch import receiver
-from allauth.account.signals import email_confirmed
-from django.contrib.auth import get_user_model
+"""
+Profiles App - Signals
+----------------
 
+Signals for Profiles App.
+    - create_profile
+"""
+
+from allauth.account.signals import email_confirmed
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import Profile
 
 
 @receiver(email_confirmed)
 def create_profile(request, email_address, **kwargs):
     """ Create a Profile when a user signs up """
-    UserModel = get_user_model()
-    user = UserModel.objects.get(emailaddress=email_address)
+    user_model = get_user_model()
+    user = user_model.objects.get(emailaddress=email_address)
     Profile.objects.create(user=user)
