202155d4bff26df9cd22212b68b7c5f08102b605
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 11:41:06 +0100

    Add module docstring and pylint disable.

--- profiles/__init__.py
+++ profiles/__init__.py
@@ -1,1 +1,13 @@
+"""
+Profiles - init
+----------------
+
+Initialisation for the profiles app.
+Required to specify the Profiles Config,
+which includes signals.
+
+"""
+
+# pylint: disable=C0103
+
 default_app_config = 'profiles.apps.ProfilesConfig'
