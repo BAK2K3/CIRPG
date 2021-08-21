f429b04f8f33289609e785aa4d632acf591ec232
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 11:53:12 +0100

    Add docstrings to forms.

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,18 @@
+"""
+Profiles App - Forms
+----------------
+
+Forms for Profiles App:
+    - HiddenForm
+"""
+
 from django import forms
 
 
 class HiddenForm(forms.Form):
+    """
+    Creates a single field which is designed
+    to be hidden, which is used for passing the
+    selected user to a view.
+    """
     user_selection = forms.CharField()
