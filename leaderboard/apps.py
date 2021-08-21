17a17acfd80503f6d228eeb3572e8111c334624e
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-08-21 12:33:47 +0100

    Add module and class docstrings.

--- leaderboard/apps.py
+++ leaderboard/apps.py
@@ -1,6 +1,14 @@
+"""
+Leaderboard App - Apps
+----------------
+
+App Configuration for Leaderboard App.
+"""
+
 from django.apps import AppConfig
 
 
 class LeaderboardConfig(AppConfig):
+    """Leaderboard App configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'leaderboard'
